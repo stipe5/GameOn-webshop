a07527ba75d27754de1601b45b3ad45ae12080ba
Stjepan <stjepan.goreta@gmail.com>
2024-03-11 19:34:10 +0100

    Inital commit - React Webshop

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { FooterComponent } from './components/footer/Footer'
+import { HeaderComponent } from './components/header/Header'
+import { MainComponent } from './components/main/Main'
+
+function App() {
+
+  return (
+    <>
+     <HeaderComponent />
+     <MainComponent />
+     <FooterComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+
+function HeaderComponent() {
+    return(
+        <div>Header</div>
+    )
+}
+
+export {HeaderComponent};
